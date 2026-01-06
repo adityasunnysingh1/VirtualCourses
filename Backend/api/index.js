1faa986ca021f8ef0a7f0c3797b9541ec051f63a
Aditya Singh <mnnitadityasingh@gmail.com>
2026-01-06 09:48:14 +0530

    cjs file added with async await

--- Backend/api/index.js
+++
@@ -1,8 +0,0 @@
-// This "Lazy Load" pattern fixes the Vercel crash
-export default async function handler(req, res) {
-  //import the app dynamically only when a request comes in
-  const appModule = await import('../index.js');
-  const app = appModule.default;
-  
-  return app(req, res);
-}
