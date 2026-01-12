ba79c3de517f48a6b24781ddd0c8db61276b001c
Aditya Singh <mnnitadityasingh@gmail.com>
2026-01-12 08:09:08 +0530

    typo and format of logout fixed

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -71,7 +71,7 @@ export const logout = async (req, res) => {
         return res.status(200).cookie("token", "", {
             maxAge: 0,              
             httpOnly: true,         
-            sameSite: 'none',       
+            sameSite: 'None',       
             secure: true            
         }).json({
             message: "Logged out successfully",
