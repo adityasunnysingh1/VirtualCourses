8dcec8bda2bdb268aee015419e1a340ed75bfcd8
Aditya Singh <mnnitadityasingh@gmail.com>
2026-01-17 18:24:27 +0530

    useEffect's return type updated

--- Frontend/src/customHooks/getCreatorCourse.js
+++ Frontend/src/customHooks/getCreatorCourse.js
@@ -9,7 +9,7 @@ const useGetCreatorCourse = () => {
     const {userData} = useSelector(state=>state.user)
   return (
     useEffect(()=>{
-       if (!userData || !userData.isAuth) return;
+       if (!userData) return;
        const creatorCourses = async()=>{
         try {
             const result = await axios.get(`${serverUrl}/api/course/getcreator`,{withCredentials:true});
