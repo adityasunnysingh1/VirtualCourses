6ab0555f185c1855df2ef7ca8f8f43728462e7fa
Aditya Singh <mnnitadityasingh@gmail.com>
2026-01-18 19:33:28 +0530

    gradient text included

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-
 import Nav from "../components/Nav_TEMP.jsx";
 import home_page from "../assets/home_page.png";
 import { SiViaplay } from "react-icons/si";
@@ -11,45 +10,88 @@ import { useNavigate } from "react-router-dom";
 import Footer from "../components/Footer.jsx";
 import About from "../components/About.jsx";
 import ReviewPage from "../components/ReviewPage.jsx";
+import GradientText from "../components/GradientText.jsx";
 
 function Home() {
   const navigate = useNavigate();
   return (
     <div className="w-full overflow-hidden">
       <div className="w-full lg:h-[140vh] h-[70vh] relative">
-        <Nav /> 
-        <img src={home_page} alt="home_page" 
-        className="object-cover md:object-fill w-full lg:h-full h-[50vh]"/>
+        <Nav />
+        <img
+          src={home_page}
+          alt="home_page"
+          className="object-cover md:object-fill w-full lg:h-full h-[50vh]"
+        />
 
-        <span className="lg:text-[70px] absolute md:text-[40px] 
-        lg:top-[10%] top-[15%] w-full flex items-center justify-center">
-        Grow your skills to advance</span>
-        <span className="lg:text-[70px] text-[20px] md:text-[40px]
+        <span
+          className="lg:text-[70px] absolute md:text-[40px] 
+        lg:top-[10%] top-[15%] w-full flex items-center justify-center"
+        >
+          Grow your skills to advance
+        </span>
+        <GradientText
+          colors={[
+            "#5227FF",
+            "#FF9FFC",
+            "#B19EEF",
+            "#ffffff",
+            "#c6d133",
+            "#7eed7f",
+            "#ffffff",
+            "#ee7744",
+          ]}
+          animationSpeed={2.5}
+          className="custom-class"
+        >
+          <span
+            className="lg:text-[70px] text-[20px] md:text-[40px]
         absolute lg:top-[18%] top-[20%] w-full flex items-center
-        justify-center text-white font-bold">Your career path</span>
-        <div className="absolute lg:top-[30%] top-[75%] md:top-[80%] w-full
-        flex items-center justify-center gap-3 flex-wrap">
-          <button className="px-[20px] py-[10px] border-2
+        justify-center text-white font-bold"
+          >
+            Your career path
+          </span>
+        </GradientText>
+
+        <div
+          className="absolute lg:top-[30%] top-[75%] md:top-[80%] w-full
+        flex items-center justify-center gap-3 flex-wrap"
+        >
+          <button
+            className="px-[20px] py-[10px] border-2
           lg:border-white border-black lg:text-white text-black rounded-[10px]
-          text-[18px] font-light flex gap-2 cursor-pointer" onClick={()=>navigate("/allcourses")}>
-          View All Courses <SiViaplay className="w-[30px] h-[30px] lg:fill-white fill-black"/>
+          text-[18px] font-light flex gap-2 cursor-pointer"
+            onClick={() => navigate("/allcourses")}
+          >
+            View All Courses{" "}
+            <SiViaplay className="w-[30px] h-[30px] lg:fill-white fill-black" />
           </button>
-          <button className="px-[20px] py-[10px] lg:bg-white bg-black 
+          <button
+            className="px-[20px] py-[10px] lg:bg-white bg-black 
           lg:text-black text-white rounded-[10px] text-[18px] font-light
-          flex gap-2 cursor-pointer items-center justify-center" onClick={()=>navigate("/search")}>
-          Search with AI
-          <img src={ai} alt="ai_icon" className="w-[30px] h-[30px] rounded-full hidden lg:block"/>
-          <img src={ai1} alt="ai1_icon" className="w-[35px] h-[35px] rounded-full lg:hidden"/>
+          flex gap-2 cursor-pointer items-center justify-center"
+            onClick={() => navigate("/search")}
+          >
+            Search with AI
+            <img
+              src={ai}
+              alt="ai_icon"
+              className="w-[30px] h-[30px] rounded-full hidden lg:block"
+            />
+            <img
+              src={ai1}
+              alt="ai1_icon"
+              className="w-[35px] h-[35px] rounded-full lg:hidden"
+            />
           </button>
         </div>
-        
       </div>
-      <Logos/>
-      <ExploreCourses/> 
-      <CardPage/>
-      <About/>
-      <ReviewPage/>
-      <Footer/>
+      <Logos />
+      <ExploreCourses />
+      <CardPage />
+      <About />
+      <ReviewPage />
+      <Footer />
     </div>
   );
 }
