d8a71915ef248db3231e1a7854a7ceb09ab7eb2d
Aditya Singh <mnnitadityasingh@gmail.com>
2026-01-22 15:39:37 +0530

    cache control enabled in getCurrentUser

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -22,6 +22,7 @@ export const getCurrentUser = async (req, res) => {
     if (!user) {
       return res.status(400).json({ message: "User not found" });
     }
+    res.set('Cache-Control', 'no-store');
     return res.status(200).json(user);
   } catch (error) {
     return res.status(500).json({ message: `getCurrentUser error ${error}` });
