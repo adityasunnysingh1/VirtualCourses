a3530a104c59800c1de2785ca4ead97ce7f9f3cc
Aditya Singh <mnnitadityasingh@gmail.com>
2026-01-25 11:44:19 +0530

    EB import don't require {} import

--- Frontend/src/pages/Educator/CreateLecture.jsx
+++ Frontend/src/pages/Educator/CreateLecture.jsx
@@ -8,7 +8,7 @@ import { setLectureData } from '../../redux/lectureSlice.js';
 import { toast } from 'react-toastify';
 import { FaEdit } from "react-icons/fa";
 import {DotPattern} from '../../components/DotPattern.jsx';
-import {ElectricBorder} from '../../components/ElectricBorder.jsx';
+import ElectricBorder from '../../components/ElectricBorder.jsx';
 import { ClipLoader } from 'react-spinners'; 
 
 function CreateLecture() {
